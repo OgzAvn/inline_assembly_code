0cb2e233a8ea311c93ca6a73b36d70fb7df170eb
Oguz <73887378+OgzAvn@users.noreply.github.com>
2021-01-26 13:13:50 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -20,6 +20,22 @@ int main(void)
     __asm volatile("LDR R1,[R2]");
     __asm volatile("ADD R0,R0,R1");
     __asm volatile("STR R0,[R2]");
+	
+    
+    int val=50;
+    __asm volatile("MOV R0,%0" : : "r"(val));  // r is meaning general purpose register  --- Input operand in this case.
+
+    int control_reg;
+    //read control register to control_reg
+    __asm volatile("MRS %0,CONTROL":"=r"(control_reg)::);
+
+    int p1,*p2;
+
+    p2=(int*)0x20000008;
+
+    __asm volatile("LDR %0,[%1]":"=r"(p1):"r"(p2));
+
+	
 
 	for(;;);
 }
